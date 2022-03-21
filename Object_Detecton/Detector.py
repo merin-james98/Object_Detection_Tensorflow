37eb339d589fa3b07bd4483de2299e0e4ab38cc6
Merin James <64535083+merin-james98@users.noreply.github.com>
2022-03-21 23:33:31 +0100

    Add files via upload

--- Object_Detecton/Detector.py
+++ Object_Detecton/Detector.py
@@ -3,9 +3,12 @@ import numpy as np
 from tensorflow.python.keras.utils.data_utils import get_file
 from numba import jit,cuda
 
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QLabel, QGridLayout, QWidget, QInputDialog
+from PyQt5.QtWidgets import QPushButton
+from PyQt5.QtCore import QSize    
 
-np.random.seed(20)
- 
+np.random.seed(20) #reproducable result all the runs
 
 class Detector:
     def __init__(self):
@@ -15,7 +18,8 @@ class Detector:
             self.classesList=f.read().splitlines()
         #color LIst
         self.colorList=np.random.uniform(low=0, high=255, size=(len(self.classesList),3))
-        
+
+    #Extracting Model   
     def downloadModel(self,modelURL):
         fileName=os.path.basename(modelURL)
         self.modelName=fileName[:fileName.index('.')]
@@ -23,16 +27,15 @@ class Detector:
         self.cacheDir="./pretrained_models"
         os.makedirs(self.cacheDir,exist_ok=True)
         get_file(fname=fileName,origin=modelURL,cache_dir=self.cacheDir,cache_subdir="checkpoints",extract=True)
-        
+
+    #load model   
     def loadModel(self):
         print("Loading Model"+self.modelName)
         tf.keras.backend.clear_session()
         self.model=tf.saved_model.load(os.path.join(self.cacheDir,"checkpoints",self.modelName,"saved_model"))
-        
-        
         print("Model"+ self.modelName+"loaded Successfully")
         
-#@jit(target="cuda")
+    #Bounding boxs around the objects  
     def createBoundigBox(self,image,threshold):
         inputTensor=cv2.cvtColor(image.copy(),cv2.COLOR_BGR2RGB)
         inputTensor=tf.convert_to_tensor(inputTensor,dtype=tf.uint8)
@@ -42,11 +45,10 @@ class Detector:
         bboxs=detections['detection_boxes'][0].numpy()
         classIndexes=detections['detection_classes'][0].numpy().astype(np.int32)
         classScores=detections['detection_scores'][0].numpy()
-        
-        
+                
         imH,imW,imC=image.shape
         
-        bboxIdx=tf.image.non_max_suppression(bboxs,classScores, max_output_size=50,iou_threshold=0.5, score_threshold=0.5)
+        bboxIdx=tf.image.non_max_suppression(bboxs,classScores, max_output_size=50,iou_threshold=0.6, score_threshold=0.6)
         
         if len(bboxs)!=0:
             for i in bboxIdx:
@@ -63,7 +65,7 @@ class Detector:
                 
                 ymin,xmin,ymax,xmax=(xmin*imW, xmax*imW,ymin*imH,ymax*imH)
                 ymin,xmin,ymax,xmax=int(xmin),int(xmax),int(ymin),int(ymax)
-                #cv2.rectangle(image,(xmin,ymin),(xmax,ymax),color=classColor,thickness=1)
+
                 cv2.putText(image,displayText,(xmax+10,ymax+20),cv2.FONT_HERSHEY_PLAIN,1,classColor,2)
                 
                 lineWidth=min(int((xmax-xmin)*0.2),int((ymax-ymin)*0.2))
@@ -78,19 +80,12 @@ class Detector:
                 cv2.line(image,(xmin,ymax),(xmin,ymax-lineWidth),classColor,thickness=5)
                 
                 cv2.line(image,(xmax,ymax),(xmax-lineWidth,ymax),classColor,thickness=5)
-                cv2.line(image,(xmax,ymax),(xmax,ymax-lineWidth),classColor,thickness=5)
-                
-                
+                cv2.line(image,(xmax,ymax),(xmax,ymax-lineWidth),classColor,thickness=5)             
         return image
                 
-                
+     #Object Detection using Webcam           
+    def predictVideo(self,videoPath,threshold): 
     
-    
-    
-
-    def predictVideo(self,videoPath,threshold):
-        
-        
         cap=cv2.VideoCapture(videoPath)
         
         cap.set(3, 1280)
@@ -108,11 +103,15 @@ class Detector:
             bboximage=self.createBoundigBox(image,threshold)
             cv2.putText(bboximage,"FPS: "+str(int(fps)),(20,70),cv2.FONT_HERSHEY_PLAIN,2,(0,255,0),2)
             
-            
-            cv2.imshow("result ", bboximage)
+            cv2.imshow("Result ", bboximage)
             key=cv2.waitKey(1) & 0xFF
             if key==ord("q"):
                 break
             (success,image)=cap.read()
+        cap.release()
         cv2.destroyAllWindows()
+        
 
+
+        
+ 
--- Object_Detecton/run_me.py
+++ Object_Detecton/run_me.py
@@ -3,11 +3,68 @@ from Detector import *
 modelURL="http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8.tar.gz"
 
 classFile="coco.names"
-threshold=0.4
-videoPath=0
+threshold=0.6
+videoPath=0 #0 for webcam
 
 detector=Detector()
 detector.readClasses(classFile)
 detector.downloadModel(modelURL)
 detector.loadModel()
-detector.predictVideo(videoPath,threshold)
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+        central_widget = QtWidgets.QWidget()
+        self.setCentralWidget(central_widget)
+
+        self.m_w11 = QtWidgets.QWidget()
+        self.m_w12 = QtWidgets.QWidget()
+        self.m_w21 = QtWidgets.QWidget()
+        self.m_w22 = QtWidgets.QWidget()
+
+        lay = QtWidgets.QGridLayout(central_widget)
+
+        for w, (r, c) in zip(
+            (self.m_w11, self.m_w12, self.m_w21, self.m_w22),
+            ((0, 0), (0, 1), (1, 0), (1, 1)),
+        ):
+            lay.addWidget(w, r, c)
+        for c in range(2):
+            lay.setColumnStretch(c, 1)
+        for r in range(2):
+            lay.setRowStretch(r, 1)
+
+        lay = QtWidgets.QVBoxLayout(self.m_w11)
+
+        self.setMinimumSize(QSize(900 , 350))    
+        self.setWindowTitle("Object Detection")
+    
+
+        pybutton1 = QPushButton('Recognize', self)
+        pybutton1.clicked.connect(self.clickMethodRecog)
+        pybutton1.resize(300,75)
+        pybutton1.move(300, 250)
+        pybutton1.setStyleSheet("""background-image: url("E:/Object_Detecton/images.jfif");border: 1px solid white;color: white;padding: 16px 32px;text-align: center;font-size: 16px;
+        margin: 4px 2px;border-radius:15px;""")
+
+    def clickMethodRecog(self):
+        detector.predictVideo(videoPath,threshold)
+
+stylesheet = """
+    MainWindow {
+        background-image: url("E:/Object_Detecton/1.jpeg"); 
+        background-repeat: no-repeat; 
+        background-position: center;
+        opacity: 0.6;
+    }
+"""
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    app.setStyleSheet(stylesheet)
+    w = MainWindow()
+    w.resize(640, 480)
+    w.show()
+    sys.exit(app.exec_())
